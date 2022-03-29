6caf58650f41e42e08babbc2d35c74b24afd145c
jdpp <juandavid162@gmail.com>
2022-03-29 13:38:32 -0500

    HTTPS

--- app/Http/Controllers/Api/LoginController.php
+++ app/Http/Controllers/Api/LoginController.php
@@ -44,7 +44,6 @@ class LoginController extends BaseController
      */
     public function login(Request $request)
     {
-        
         if(Auth::attempt(['email' => $request->email, 'password' => $request->password])){ 
             $user = Auth::user(); 
             $success['token'] =  $user->createToken('MyApp')->accessToken; 
