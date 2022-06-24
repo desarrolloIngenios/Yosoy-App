c5528b47624e11f5e5eda8f6b3307367eff8c06c
jdpp <juandavid162@gmail.com>
2022-06-23 19:13:44 -0500

    updated

--- app/Http/Controllers/Api/CompanyController.php
+++ app/Http/Controllers/Api/CompanyController.php
@@ -27,4 +27,24 @@ class CompanyController extends BaseController
             )->get();
         return $this->sendResponse($empresa, 'empresa');
     }
+
+    public function update(Request $request)
+    {
+        $input = $request->all();
+        //$user = $request->user();
+        $empresa = Empresa::find($input['id']);
+        $empresa->fill($request->except(['_token']));
+        //$empresa->user_id = $user->id;
+        $empresa->save();
+        
+        return $this->sendResponse($empresa, 'Empresa');
+    }
+
+    public function find(Request $request, $id)
+    {
+        $empresa = Empresa::find($id);
+        
+        return $this->sendResponse($empresa, 'Empresa');
+    }
+
 }
--- resources/views/empresa/edit.blade.php
+++ resources/views/empresa/edit.blade.php
@@ -10,21 +10,21 @@
                 <p class="mb-2"></p>
             </div>
             <div class="card-body pt-0">
-                <form action="{{ route('empresa_create.post') }}" method="post">
+                <form action="{{ route('empresa_update.post') }}" method="post">
                     @csrf
-                    <!-- <div class="">
-
+                     <div class="">
+                     <input id="id" name="id" type="hidden" value="{{$empresa['id']}}">
                         <div class="row row-sm">
                             <div class="col-lg-6">
                                 <div class="form-group">
                                     <label for="nombre">Empresa ( Razón social ) o nombre del servicio</label>
-                                    <input name="nombre" class="form-control" value="{{isset($perfil['nombre'])? $perfil['nombre']:''}}" placeholder="Empresa ( Razón social ) o nombre del servicio" type="text" required>
+                                    <input name="nombre" class="form-control" value="{{isset($empresa['nombre'])? $empresa['nombre']:''}}" placeholder="Empresa ( Razón social ) o nombre del servicio" type="text" required>
                                 </div>
                             </div>
                             <div class="col-lg-6">
                                 <div class="form-group">
                                     <label for="nit">Nit</label>
-                                    <input name="nit" class="form-control" value="{{isset($perfil['nit'])? $perfil['nit']:''}}" placeholder="Nit" type="text" required>
+                                    <input name="nit" class="form-control" value="{{isset($empresa['nit'])? $empresa['nit']:''}}" placeholder="Nit" type="text" required>
                                 </div>
                             </div>
                         </div>
@@ -33,14 +33,14 @@
                             <div class="col-lg-6">
                                 <div class="form-group">
                                     <label for="direccion">Dirección</label>
-                                    <input name="direccion" class="form-control" value="{{isset($perfil['direccion'])? $perfil['direccion']:''}}" placeholder="Dirección" type="text" required>
+                                    <input name="direccion" class="form-control" value="{{isset($empresa['direccion'])? $empresa['direccion']:''}}" placeholder="Dirección" type="text" required>
                                 </div>
                             </div>
                             <div class="col-lg-6 mg-b-6 mg-lg-b-0">
                                 <p class="mg-b-10">Ciudad</p><select class="form-control select2" name="ciudad_id" placeholder="Ciudad" required>
                                     <option value=""></option>
                                     @foreach($ciudades as $ciudad)
-                                        @if(isset($perfil['ciudad_id']) && $perfil['ciudad_id'] == $ciudad['id'])
+                                        @if(isset($empresa['ciudad_id']) && $empresa['ciudad_id'] == $ciudad['id'])
                                             <option value="{{ $ciudad['id'] }}" selected>
                                                 {{ $ciudad['pais_departamento_ciudad'] }}
                                             </option>
@@ -62,17 +62,21 @@
                             <div class="col-lg-6">
                                 <div class="form-group">
                                     <label for="numero_contacto">Número de contacto</label>
-                                    <input name="numero_contacto" class="form-control" value="{{isset($perfil['numero_contacto'])? $perfil['numero_contacto']:''}}" placeholder="Número contacto" name="numero_contacto_1" type="text"  required>
+                                    <input name="numero_contacto" class="form-control" value="{{isset($empresa['numero_contacto'])? $empresa['numero_contacto']:''}}" placeholder="Número contacto" name="numero_contacto_1" type="text"  required>
                                 </div>
                             </div>
-                           
+                            <div class="col-lg-6">
+                                <div class="form-group">
+                                    <label>Correo para factura electrónica</label> <input class="form-control" name="email_factura_electronica" placeholder="Correo para factura electrónica" value="{{isset($empresa['email_factura_electronica'])? $empresa['email_factura_electronica']:''}}" type="email" required>
+                                </div>
+                            </div>
                         </div>
 
                         <div class="row row-sm">
                             <div class="col-lg-12">
                                 <div class="form-group">
                                     <label for="descripcion">Descripción</label>
-                                    <textarea type="text" name="descripcion" class="form-control" value="{{isset($perfil['descripcion'])? $perfil['descripcion']:''}}" placeholder="Descripción" name="numero_contacto_1" type="text"  required></textarea>
+                                    <textarea type="text" name="descripcion" class="form-control" placeholder="Descripción"  required>{{isset($empresa['descripcion'])? $empresa['descripcion']:''}}</textarea>
                                 </div>
                             </div>
                           
@@ -83,7 +87,7 @@
                         </div>
 
                     </div>
-                    <button type="submit" class="btn btn-primary mt-3 mb-0">Crear</button> -->
+                    <button type="submit" class="btn btn-primary mt-3 mb-0">Guardar</button> 
                 </form>
 
                 
