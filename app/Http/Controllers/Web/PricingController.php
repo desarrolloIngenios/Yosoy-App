c500337a79957def081a549a68ddd39f5da77d26
jdpp <juandavid162@gmail.com>
2022-12-15 10:00:00 -0500

    updated

--- app/Http/Controllers/Web/PricingController.php
+++ app/Http/Controllers/Web/PricingController.php
@@ -11,7 +11,13 @@ class PricingController extends Controller
 {
     public function index(Request $request)
     {
-        $data = [];
+        $public_key_wompi = "pub_test_WFaWpZ8xYKWD8x0Vs713YNYADtuQePwL";
+        if(env('APP_ENV') == 'production'){
+            $public_key_wompi = "pub_prod_QVZBgzXWjsT1EuCDmzUWyyJKDyYdlvx9";
+        }
+        $data = [
+            'public_key_wompi' => $public_key_wompi
+        ];
         return view('pricing/index', $data);
     }
 
