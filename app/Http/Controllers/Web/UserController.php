1ffe2e13758f63557f52fa495097470a71c1d851
jdpp <juandavid162@gmail.com>
2023-04-27 06:05:20 -0500

    update

--- app/Http/Controllers/Web/UserController.php
+++ app/Http/Controllers/Web/UserController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+
 
 class UserController extends Controller
 {
@@ -14,6 +16,18 @@ class UserController extends Controller
         $data = [];
         $response = Http::withToken(session('token'))->accept('application/json')->get(route('api.user_list'), []);
         //dd($response->json());
+        if(!is_null($response)){
+            if (!array_key_exists('success', $response->json())) {
+                // $debugInfo = debug_backtrace(DEBUG_BACKTRACE_PROVIDE_OBJECT, 1)[0];
+                // Log::info($debugInfo);
+                Log::info('Contenido de la sesión: ' . json_encode(session()->all()));
+                Log::info("Método ".__METHOD__." en linea ".__LINE__);
+                return redirect(\Request::url());
+                //return redirect()->back();
+            }
+        } else {
+            return redirect()->back();
+        }
         $success = $response->json()['success'];
         $users = $response->json()['data'];
         $message = $response->json()['message'];
