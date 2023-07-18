5b1e4626c8531a07cadc13acc19e70995b5ab87b
jdpp <juandavid162@gmail.com>
2023-07-18 06:37:31 -0500

    pricing index

--- app/Http/Controllers/Web/PricingController.php
+++ app/Http/Controllers/Web/PricingController.php
@@ -11,7 +11,7 @@ use App\Models\DescuentosOfertas;
 
 class PricingController extends Controller
 {
-    public function index(Request $request)
+    public function index(Request $request, $tipo_candidato = null, $valor_ingresado = 0)
     {
         $public_key_wompi = "pub_test_WFaWpZ8xYKWD8x0Vs713YNYADtuQePwL";
         if(env('APP_ENV') == 'production'){
@@ -26,7 +26,25 @@ class PricingController extends Controller
         $data['descuentos'] = $descuentos;
         $data['precio_base'] = $precio_base;
 
+        $data['tipo_candidato'] = $tipo_candidato;
+        $data['empirico_valor'] = 50000;
+        $data['tecnico_valor'] = 135000;
+
+        if($tipo_candidato == 'empirico' && $valor_ingresado > $data['empirico_valor']){
+            $data['empirico_valor'] = $valor_ingresado;
+        } else if($tipo_candidato == 'tecnico' && $valor_ingresado > $data['tecnico_valor']){
+            $data['tecnico_valor'] = $valor_ingresado;
+        }
+
         return view('pricing/index', $data);
+    }
+
+    function edit_valor_pagar(Request $request)
+    {
+        $valor_ingresado = $request->input('quantity');
+        $tipo_candidato = $request->input('tipo_candidato');
+
+        return redirect()->route('pricing.index',['tipo_candidato' => $tipo_candidato, 'valor_ingresado' => $valor_ingresado]);
     }
 
 }
