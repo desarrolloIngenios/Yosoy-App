43a5433e59b804f12ac9c76807c91e173f3c20e9
Jose bohorquez <bd567358546@gmail.com>
2024-05-23 11:01:10 -0500

    mandando cambio login title

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -12,7 +12,7 @@
         content="admin,admin dashboard,admin dashboard template,admin panel template,admin template,admin theme,bootstrap 4 admin template,bootstrap 4 dashboard,bootstrap admin,bootstrap admin dashboard,bootstrap admin panel,bootstrap admin template,bootstrap admin theme,bootstrap dashboard,bootstrap form template,bootstrap panel,bootstrap ui kit,dashboard bootstrap 4,dashboard design,dashboard html,dashboard template,dashboard ui kit,envato templates,flat ui,html,html and css templates,html dashboard template,html5,jquery html,premium,premium quality,sidebar bootstrap 4,template admin bootstrap 4" />
 
     <!-- Title -->
-    <title> Yo Soy </title>
+    <title> Yo Soy - test </title>
 
     <!-- Favicon -->
     <link rel="icon" href="../../images/Logo1.png" type="image/x-icon" />
@@ -125,17 +125,17 @@
 														<p>Info: User pending action</p>
 														<button class="close-btn"><span class="close_icon"></span></button>
 													</div>
-												
+
 													<div class="message-box warning">
 														<p>Warning: User has to be admin</p>
 														<button class="close-btn"><span class="close_icon"></span></button>
 													</div>
-												
+
 													<div class="message-box error">
 														<p>Error: Internal Server Error</p>
 														<button class="close-btn"><span class="close_icon"></span></button>
 													</div>
-												
+
 													<div class="message-box success">
 														<p>Success: Updated members status</p>
 														<button class="close-btn"><span class="close_icon"></span></button>
